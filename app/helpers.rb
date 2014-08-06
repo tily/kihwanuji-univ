a9256a7c2f4ca73a778891484adaae0c17ce7cee
tily <tily05@gmail.com>
2014-08-06 22:13:00 +0900

    use fragmental views

--- app/helpers.rb
+++ app/helpers.rb
@@ -4,6 +4,10 @@ KihwanujiUniv::App.helpers do
     @current_account ||= Account.where(uid: session[:uid]).first
   end
 
+  def list_item(model, object)
+    haml(:"fragments/#{model}", layout: false, locals: {model => object})
+  end
+
   def timetable(lectures)
     haml(:"fragments/timetable", layout: false, locals: {lectures: lectures})
   end
